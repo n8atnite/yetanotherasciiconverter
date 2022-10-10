873ca89de5e5250b16172c32931e08d92d09eade
Nate Atkinson <atkies@ucla.edu>
2022-10-09 19:39:26 -0700

    font dropdown + console redirect

--- yaac.py
+++ yaac.py
@@ -8,6 +8,13 @@ import numpy as np
 from tqdm import tqdm
 import argparse
 
+from tkinter import *
+from tkinter import font, ttk
+from matplotlib import font_manager
+
+BASE_DPI = 72
+SCALE = 1
+
 ERRORS = {
     "invalid_ext": "Not a valid file extension.",
     "invalid_in": "Not a valid input file path.",
@@ -154,32 +161,118 @@ def convert(frames, properties, font, resize=True):
 
     return new_frames
 
+def scaled(width):
+    return round(width*SCALE)
+
+def filter_font_props(props):
+    accepted = ('family', 'slant', 'size', 'weight')
+    filtered = {}
+    for key, value in props.items():
+        if key in accepted:
+            if key == 'slant':
+                if value == 'roman':
+                    filtered['style'] = 'normal'
+                else:
+                    filtered['style'] = value
+            else:
+                filtered[key] = value
+    return filtered
+
+def load_style():
+    style = ttk.Style()
+    style_settings = (
+        ('TCombobox', {
+            'width': scaled(30),
+            'arrowsize': scaled(30),
+            # 'postoffset': (0, 0, 100, 0)
+        }),
+        ('Vertical.TScrollbar', {
+            'width': scaled(30),
+            'arrowsize': scaled(30)
+        }),
+    )
+    for setting in style_settings:
+        style.configure(setting[0], **setting[1])
+
+    return style
+
+class PrintLogger(): # create file like object
+    def __init__(self, textbox): # pass reference to text widget
+        self.textbox = textbox # keep ref
+
+    def write(self, text):
+        self.textbox.insert(END, text) # write text to textbox
+            # could also scroll to end of textbox here to make sure always visible
+
+    def flush(self): # needed for file like object
+        pass
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='''
-     __    __  ______  ______  ____      
-    /\ \  /\ \/\  _  \/\  _  \/\  _`\    
-    \ `\`\\\/'/\ \ \L\ \ \ \L\ \ \ \/\_\  
-     `\ `\ /'  \ \  __ \ \  __ \ \ \/_/_ 
-       `\ \ \   \ \ \/\ \ \ \/\ \ \ \L\ \\
-         \ \_\   \ \_\ \_\ \_\ \_\ \____/
-          \/_/    \/_/\/_/\/_/\/_/\/___/ 
+    # parser = argparse.ArgumentParser(description='''
+    #  __    __  ______  ______  ____      
+    # /\ \  /\ \/\  _  \/\  _  \/\  _`\    
+    # \ `\`\\\/'/\ \ \L\ \ \ \L\ \ \ \/\_\  
+    #  `\ `\ /'  \ \  __ \ \  __ \ \ \/_/_ 
+    #    `\ \ \   \ \ \/\ \ \ \/\ \ \ \L\ \\
+    #      \ \_\   \ \_\ \_\ \_\ \_\ \____/
+    #       \/_/    \/_/\/_/\/_/\/_/\/___/ 
                                         
-    Yet Another Ascii Converter by n8atnite
-    convert .mp4 or .avi to asciified .mp4
-    ''', formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-t', '--fontpath', default=FONTS_PATH+FONT, help='font/typeface filepath (.ttf)')
-    parser.add_argument('inpath', help='full video file path (including name and ext)')
-    parser.add_argument('-o', '--outpath', default='ascii.mp4', help='output video path (including name and .mp4/.mkv ext)')
-    args = parser.parse_args()
+    # Yet Another Ascii Converter by n8atnite
+    # convert .mp4 or .avi to asciified .mp4
+    # ''', formatter_class=argparse.RawTextHelpFormatter)
+    # parser.add_argument('-t', '--fontpath', default=FONTS_PATH+FONT, help='font/typeface filepath (.ttf)')
+    # parser.add_argument('inpath', help='full video file path (including name and ext)')
+    # parser.add_argument('-o', '--outpath', default='ascii.mp4', help='output video path (including name and .mp4/.mkv ext)')
+    # args = parser.parse_args()
 
-    if not os.path.exists(args.inpath):
-        raise FileNotFoundError(ERRORS["invalid_in"])
-    if not os.path.exists(os.path.split(os.path.abspath(args.outpath))[0]):
-        raise FileNotFoundError(ERRORS["invalid_out"])
-    if not os.path.exists(args.fontpath):
-        raise FileNotFoundError(ERRORS["invalid_font"])
+    # if not os.path.exists(args.inpath):
+    #     raise FileNotFoundError(ERRORS["invalid_in"])
+    # if not os.path.exists(os.path.split(os.path.abspath(args.outpath))[0]):
+    #     raise FileNotFoundError(ERRORS["invalid_out"])
+    # if not os.path.exists(args.fontpath):
+    #     raise FileNotFoundError(ERRORS["invalid_font"])
 
-    frames, properties = load_file(args.inpath)
-    font = get_font(args.fontpath, properties['c_pixel_size'])
-    out = convert(frames, properties, font)
-    write_file(out, args.outpath, properties)
+    root = Tk()
+    style = load_style()
+    SCALE = root.winfo_fpixels('1i')/BASE_DPI
+    rootFont = font.Font(family='TkDefaultFont', size=24)
+
+    root.title('YAAC')
+    root.pack_propagate(0)
+    root.focus()
+    root.geometry(str(scaled(800)) + "x" + str(scaled(600)+7) + "+" + str(-7) + "+" + str(0))
+
+    fontFamilies = sorted(list(font.families()))
+    fontMenuVar = StringVar(root)
+    fontMenuVar.set(fontFamilies[0])
+    fontMenu = ttk.Combobox(
+        root, 
+        textvariable=fontMenuVar, 
+        values=fontFamilies, 
+        font=rootFont, 
+        style='font.TCombobox',
+    )
+    fontMenu.pack()
+
+    consoleBox = Text()
+    consoleBox.pack()
+    log = PrintLogger(consoleBox)
+    sys.stdout = log
+
+    def report_font(*args):
+        fontMenu.configure(font=font.Font(family=fontMenu.get(), size=24))
+        p = font_manager.FontProperties(**filter_font_props(font.Font(font=fontMenu['font']).actual()))
+        try:
+            path = font_manager.findfont(p, fallback_to_default=False)
+        except ValueError:
+            path = "ERROR: FONT PATH NOT FOUND"
+        print(path)
+
+    fontMenu.bind("<<ComboboxSelected>>", report_font)
+
+    # frames, properties = load_file(args.inpath)
+    # font = get_font(args.fontpath, properties['c_pixel_size'])
+    # out = convert(frames, properties, font)
+    # write_file(out, args.outpath, properties)
+
+    root.mainloop()
